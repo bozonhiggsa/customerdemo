ea99cef4315eae5a5ab9a796236ab59d43556414
Ihor Savchenko <ihor.v.savchenko@gmail.com>
2019-05-14 23:17:09 +0300

    add new branch

--- src/main/resources/database/initDB.sql
+++ src/main/resources/database/initDB.sql
@@ -1,7 +1,13 @@
-CREATE TABLE IF NOT EXISTS customers (
-  id         BIGINT AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(50)   NOT NULL,
-  last_name  VARCHAR(100)  NOT NULL,
-  address    VARCHAR(1000) NOT NULL,
-  budget     DECIMAL       NOT NULL
-);
+CREATE TABLE db_for_spring_rest.public.customers
+(
+  ID serial NOT NULL,
+  first_name character varying(50) NOT NULL,
+  last_name character varying(100) NOT NULL,
+  address character varying(1000) NOT NULL,
+  budget DECIMAL NOT NULL,
+  CONSTRAINT ID PRIMARY KEY (ID)
+)
+WITH (
+OIDS = FALSE
+)
+;
